335a93157330367a277a0cad9dab0fae6f356f41
smael mahmodeyan <smael@aftersearch.com>
2026-06-25 17:13:45 +0330

    Add Ariana DI package

---
+++ packages/core/src/di/index.ts
@@ -0,0 +1,5 @@
+export { Injector, inject, runInInjectionContext } from './injector.js';
+export { createToken } from './token.js';
+export { provide } from './provider.js';
+export type { Token, Constructor } from './token.js';
+export type { Provider, ClassProvider, ValueProvider, FactoryProvider } from './provider.js';
