7170a53d0de8e356c60cb51100dad88dc7f94f0a
smael mahmodeyan <smael@aftersearch.com>
2026-06-25 20:32:19 +0330

    Export listSignal from core reactivity

--- packages/core/src/reactivity/index.ts
+++ packages/core/src/reactivity/index.ts
@@ -1,4 +1,6 @@
 export { signal } from './signal.js';
 export { computed } from './computed.js';
 export { effect } from './effect.js';
+export { listSignal } from './list-signal.js';
 export type { Signal, Subscriber, Cleanup } from './types.js';
+export type { ListSignal, ListChange, ListChangeSubscriber, ListKey } from './list-signal.js';
