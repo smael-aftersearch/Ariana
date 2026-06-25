56e3c6996a4020e0e9aa2236ab677313a9b61682
smael mahmodeyan <smael@aftersearch.com>
2026-06-25 17:12:06 +0330

    Add Ariana reactivity core

---
+++ packages/core/src/reactivity/index.ts
@@ -0,0 +1,4 @@
+export { signal } from './signal.js';
+export { computed } from './computed.js';
+export { effect } from './effect.js';
+export type { Signal, Subscriber, Cleanup } from './types.js';
