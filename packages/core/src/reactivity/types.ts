18ba0b8b2bb961b1d29b6b52bce63f7199c37090
smael mahmodeyan <smael@aftersearch.com>
2026-06-25 17:10:41 +0330

    Fix Ariana reactivity types

--- packages/core/src/reactivity/types.ts
+++ packages/core/src/reactivity/types.ts
@@ -1,9 +1,10 @@
-export type Subscriber = () => void;
 export type Cleanup = () => void;
+export type Subscriber = () => void;
 
 export type Signal<T> = {
   (): T;
   set(value: T): void;
   update(updater: (value: T) => T): void;
+  peek(): T;
   subscribe(subscriber: Subscriber): Cleanup;
 };
