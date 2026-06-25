cba10adafebe3c415ac5e7aaa27f304b32dd23f5
smael mahmodeyan <smael@aftersearch.com>
2026-06-25 17:14:49 +0330

    Add Ariana component model

---
+++ packages/core/src/component/index.ts
@@ -0,0 +1,4 @@
+export { Component, Route, getComponentMetadata, getRouteMetadata } from './component.js';
+export { bootstrap } from './bootstrap.js';
+export type { BootstrapOptions } from './bootstrap.js';
+export type { ComponentOptions, RouteOptions, ComponentType } from './metadata.js';
