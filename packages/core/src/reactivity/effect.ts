cf59302604ce84ce35ad28a70ae4db95c64bcc39
smael mahmodeyan <smael@aftersearch.com>
2026-06-26 19:35:00 +0330

    Support cleanup callbacks in effects

--- packages/core/src/reactivity/effect.ts
+++ packages/core/src/reactivity/effect.ts
@@ -1,11 +1,23 @@
 import { ReactiveComputation } from './runtime.js';
 import type { Cleanup } from './types.js';
 
-export function effect(callback: () => void): Cleanup {
-  const computation = new ReactiveComputation(callback);
+export type EffectCallback = () => void | Cleanup;
+
+export function effect(callback: EffectCallback): Cleanup {
+  let userCleanup: Cleanup | undefined;
+
+  const computation = new ReactiveComputation(() => {
+    userCleanup?.();
+    userCleanup = undefined;
+    const result = callback();
+    if (typeof result === 'function') userCleanup = result;
+  });
+
   computation.run();
 
   return () => {
     computation.cleanup();
+    userCleanup?.();
+    userCleanup = undefined;
   };
 }
