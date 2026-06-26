2a857cf6fc95625a49587c61b47f0802c3c915b6
smael mahmodeyan <smael@aftersearch.com>
2026-06-26 19:54:30 +0330

    Export bootstrap application API

--- packages/core/src/component/index.ts
+++ packages/core/src/component/index.ts
@@ -1,4 +1,4 @@
 export { Component, Route, getComponentMetadata, getRouteMetadata } from './component.js';
-export { bootstrap } from './bootstrap.js';
-export type { BootstrapOptions } from './bootstrap.js';
+export { bootstrap, bootstrapApplication } from './bootstrap.js';
+export type { BootstrapOptions, BootstrapRef } from './bootstrap.js';
 export type { ComponentOptions, RouteOptions, ComponentType } from './metadata.js';
