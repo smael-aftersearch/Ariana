186659ec6907a532f6c0cd2d6791931499e18993
smael mahmodeyan <smael@aftersearch.com>
2026-06-26 19:35:22 +0330

    Export cleanup scope primitive

--- packages/core/src/reactivity/index.ts
+++ packages/core/src/reactivity/index.ts
@@ -2,5 +2,7 @@ export { signal } from './signal.js';
 export { computed } from './computed.js';
 export { effect } from './effect.js';
 export { listSignal } from './list-signal.js';
+export { createCleanupScope } from './scope.js';
 export type { Signal, Subscriber, Cleanup } from './types.js';
 export type { ListSignal, ListChange, ListChangeSubscriber, ListKey } from './list-signal.js';
+export type { CleanupScope } from './scope.js';
