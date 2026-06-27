369008db8bcd38cb28b7294235f0f8296532230e
smael mahmodeyan <smael@aftersearch.com>
2026-06-28 03:25:17 +0330

    Diff reactive dependencies between runs

--- packages/core/src/reactivity/runtime.ts
+++ packages/core/src/reactivity/runtime.ts
@@ -5,37 +5,55 @@ export type Dependency = {
 };
 
 export class ReactiveComputation {
-  private cleanups: Cleanup[] = [];
+  private cleanups = new Map<Dependency, Cleanup>();
   private dependencies = new Set<Dependency>();
+  private nextDependencies = new Set<Dependency>();
 
   constructor(private readonly callback: () => void) {}
 
   run = () => {
-    this.cleanup();
+    this.nextDependencies.clear();
     pushComputation(this);
 
     try {
       this.callback();
     } finally {
       popComputation();
+      this.reconcileDependencies();
     }
   };
 
   track(dependency: Dependency) {
+    this.nextDependencies.add(dependency);
+
     if (this.dependencies.has(dependency)) {
       return;
     }
 
     this.dependencies.add(dependency);
-    this.cleanups.push(dependency.subscribe(this.run));
+    this.cleanups.set(dependency, dependency.subscribe(this.run));
   }
 
   cleanup() {
-    for (const cleanup of this.cleanups.splice(0)) {
+    for (const cleanup of this.cleanups.values()) {
       cleanup();
     }
 
+    this.cleanups.clear();
     this.dependencies.clear();
+    this.nextDependencies.clear();
+  }
+
+  private reconcileDependencies() {
+    for (const dependency of this.dependencies) {
+      if (this.nextDependencies.has(dependency)) {
+        continue;
+      }
+
+      this.cleanups.get(dependency)?.();
+      this.cleanups.delete(dependency);
+      this.dependencies.delete(dependency);
+    }
   }
 }
 
