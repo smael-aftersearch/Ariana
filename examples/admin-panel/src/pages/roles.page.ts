da4466434d271e0104e5e95454cf66f949911579
smael mahmodeyan <smael@aftersearch.com>
2026-06-28 23:42:36 +0330

    Add lazy roles page module

---
+++ examples/admin-panel/src/pages/roles.page.ts
@@ -0,0 +1,4 @@
+import { Component, Route } from '@ariana/core';
+@Route('/roles')
+@Component({ selector: 'ari-admin-roles-page', templateUrl: './roles.page.html', styleUrl: './roles.page.css' })
+export class RolesPage {}
