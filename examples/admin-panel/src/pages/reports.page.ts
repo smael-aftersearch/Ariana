60c756488399230378908019052eda8ca98e1b2e
smael mahmodeyan <smael@aftersearch.com>
2026-06-28 23:44:14 +0330

    Add lazy reports page module

---
+++ examples/admin-panel/src/pages/reports.page.ts
@@ -0,0 +1,4 @@
+import { Component, Route } from '@ariana/core';
+@Route('/reports')
+@Component({ selector: 'ari-admin-reports-page', templateUrl: './reports.page.html', styleUrl: './reports.page.css' })
+export class ReportsPage {}
