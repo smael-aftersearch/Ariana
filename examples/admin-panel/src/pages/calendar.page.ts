06a490d57663fd60ddc75cb0e4cc94df65bb3c6d
smael mahmodeyan <smael@aftersearch.com>
2026-06-28 23:44:57 +0330

    Add lazy schedule page module

---
+++ examples/admin-panel/src/pages/calendar.page.ts
@@ -0,0 +1,4 @@
+import { Component, Route } from '@ariana/core';
+@Route('/calendar')
+@Component({ selector: 'ari-admin-schedule-page', templateUrl: './calendar.page.html', styleUrl: './calendar.page.css' })
+export class CalendarPage {}
