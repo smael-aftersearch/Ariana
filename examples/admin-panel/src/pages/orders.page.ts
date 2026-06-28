f0cd9fa763fbefb107a17d5e6fa1a4f117ee2a14
smael mahmodeyan <smael@aftersearch.com>
2026-06-28 23:43:34 +0330

    Add lazy orders page module

---
+++ examples/admin-panel/src/pages/orders.page.ts
@@ -0,0 +1,4 @@
+import { Component, Route } from '@ariana/core';
+@Route('/orders')
+@Component({ selector: 'ari-admin-orders-page', templateUrl: './orders.page.html', styleUrl: './orders.page.css' })
+export class OrdersPage {}
