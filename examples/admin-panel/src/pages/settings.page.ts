804229e26bb177552e0bbabd1eb1433cb005e2b0
smael mahmodeyan <smael@aftersearch.com>
2026-06-28 23:45:35 +0330

    Add lazy settings page module

---
+++ examples/admin-panel/src/pages/settings.page.ts
@@ -0,0 +1,4 @@
+import { Component, Route } from '@ariana/core';
+@Route('/settings')
+@Component({ selector: 'ari-admin-settings-page', templateUrl: './settings.page.html', styleUrl: './settings.page.css' })
+export class SettingsPage {}
