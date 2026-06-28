a8cd8addb5e67da32ec82c7846706ff76960298c
smael mahmodeyan <smael@aftersearch.com>
2026-06-28 23:40:33 +0330

    Add lazy dashboard page module

---
+++ examples/admin-panel/src/pages/dashboard.page.ts
@@ -0,0 +1,5 @@
+import { Component, Route } from '@ariana/core';
+
+@Route('/')
+@Component({ selector: 'ari-admin-dashboard-page', templateUrl: './dashboard.page.html', styleUrl: './dashboard.page.css' })
+export class DashboardPage {}
