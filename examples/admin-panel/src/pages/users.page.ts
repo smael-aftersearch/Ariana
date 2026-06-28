31aa98caf52ef1a6ef838b0292acc5d379b840ba
smael mahmodeyan <smael@aftersearch.com>
2026-06-28 23:41:56 +0330

    Add lazy users page module

---
+++ examples/admin-panel/src/pages/users.page.ts
@@ -0,0 +1,4 @@
+import { Component, Route } from '@ariana/core';
+@Route('/users')
+@Component({ selector: 'ari-admin-users-page', templateUrl: './users.page.html', styleUrl: './users.page.css' })
+export class UsersPage {}
