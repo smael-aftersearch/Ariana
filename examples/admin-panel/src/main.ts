c0085d918cc170edee7954638a190eea3d7b7e65
smael mahmodeyan <smael@aftersearch.com>
2026-06-29 01:46:06 +0330

    Bootstrap admin animation lab from query flag

--- examples/admin-panel/src/main.ts
+++ examples/admin-panel/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap, provide } from '@ariana/core';
 import { createQueryClient } from '@ariana/query';
 import { AdminPanelPage } from './admin-panel.page.js';
+import { AdminAnimationLabPage } from './animation-lab.page.js';
 import { ADMIN_QUERY_CLIENT } from './app-tokens.js';
 import { AdminApiService } from './services/admin-api.service.js';
 import { AuthService } from './services/auth.service.js';
@@ -8,7 +9,9 @@ import { I18nService } from './services/i18n.service.js';
 import './admin-panel.extra.css';
 import './admin-animations.scss';
 
-bootstrap(AdminPanelPage, '#app', {
+const showAnimationLab = new URLSearchParams(window.location.search).get('lab') === 'animation';
+
+bootstrap(showAnimationLab ? AdminAnimationLabPage : AdminPanelPage, '#app', {
   providers: [
     AdminApiService,
     AuthService,
