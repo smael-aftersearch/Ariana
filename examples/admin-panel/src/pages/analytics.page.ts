b91a1a73ac13d5bf1574f09dde3dae8cf9268a54
smael mahmodeyan <smael@aftersearch.com>
2026-06-28 23:41:14 +0330

    Add lazy analytics page module

---
+++ examples/admin-panel/src/pages/analytics.page.ts
@@ -0,0 +1,4 @@
+import { Component, Route } from '@ariana/core';
+@Route('/analytics')
+@Component({ selector: 'ari-admin-analytics-page', templateUrl: './analytics.page.html', styleUrl: './analytics.page.css' })
+export class AnalyticsPage {}
