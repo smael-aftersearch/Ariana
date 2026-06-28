cb85434a0286a848e68b25fb23d3c92413ad4b98
smael mahmodeyan <smael@aftersearch.com>
2026-06-28 23:43:07 +0330

    Add lazy products page module

---
+++ examples/admin-panel/src/pages/products.page.ts
@@ -0,0 +1,4 @@
+import { Component, Route } from '@ariana/core';
+@Route('/products')
+@Component({ selector: 'ari-admin-products-page', templateUrl: './products.page.html', styleUrl: './products.page.css' })
+export class ProductsPage {}
