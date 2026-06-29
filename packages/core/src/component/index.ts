97e036cd571e22a3a6fa265ae849ae7eb217accf
smael mahmodeyan <smael@aftersearch.com>
2026-06-30 02:16:27 +0330

    Export component mount helper

--- packages/core/src/component/index.ts
+++ packages/core/src/component/index.ts
@@ -1,4 +1,4 @@
 export { Component, Route, getComponentMetadata, getRouteMetadata } from './component.js';
-export { bootstrap, bootstrapApplication } from './bootstrap.js';
-export type { BootstrapOptions, BootstrapRef } from './bootstrap.js';
+export { bootstrap, bootstrapApplication, mountComponent } from './bootstrap.js';
+export type { BootstrapOptions, BootstrapRef, MountOptions, MountRef } from './bootstrap.js';
 export type { ComponentOptions, RouteOptions, ComponentType } from './metadata.js';
